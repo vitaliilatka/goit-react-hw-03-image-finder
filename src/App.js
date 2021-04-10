8754e998a8ca2502f6f68af2b5e575a08a991db6
vetallatka <latka.vitalik@yandex.ru>
2021-04-10 07:32:30 +0200

    added searchbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import Searchbar from './Components/Searchbar/Searchbar';
 import Container from './Components/Container/Container';
 import './styles.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [largeImgUrl, setLargeImgUrl] = useState('');
+
+  useEffect(() => {
+    if (!searchQuery) return;
+    fetchArticles();
+  }, [searchQuery]);
+
+  const onChangeQuery = query => {
+    setSearchQuery(query);
+    setImages([]);
+    setCurrentPage(1);
+    setError(null);
+  };
+
+  const fetchArticles = () => {
+    const options = { currentPage, searchQuery };
+    setIsLoading(true);
+
+    searchImgApi
+      .fetchImages(options)
+      .then(hits => {
+        const imgData = hits.map(({ id, webformatURL, largeImageURL }) => ({
+          id,
+          webformatURL,
+          largeImageURL,
+        }));
+        setImages(prState => [...prState, ...imgData]);
+        setCurrentPage(prState => prState + 1);
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      })
+      .catch(error => setError(error))
+      .finally(() => setIsLoading(false));
+  };
+
+  const openModal = e => {
+    if (e.target.nodeName !== 'IMG') return;
+    setLargeImgUrl(e.target.dataset.url);
+  };
+
+  const closeModal = () => {
+    setLargeImgUrl('');
+  };
+
+  const shouldRenderLoadMoreBtn = images.length > 0 && !isLoading;
 
   return (
-    <Container>
-
-    </Container>
+    <div className="App">
+      <Searchbar onSubmit={onChangeQuery} />
+      {error && <h2>Ой, ошибка, все пропало (((</h2>}
+      <ImageGallery images={images} onClick={openModal} />
+      {isLoading && <Loader />}
+      {shouldRenderLoadMoreBtn && <Button onClick={fetchArticles} />}
+      {largeImgUrl && (
+        <Modal onClose={closeModal}>
+          <img src={largeImgUrl} alt="Большое изображение" />
+        </Modal>
+      )}
+    </div>
   );
 }
 
--- src/Components/Searchbar/Searchbar.jsx
+++ src/Components/Searchbar/Searchbar.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const ApiKey = https://pixabay.com/api/?q=что_искать&page=номер_страницы&key=20273026-5f41ec9ec9512e6f61de79333&image_type=photo&orientation=horizontal&per_page=12
 
-<header className="Searchbar">
-    <form className="SearchForm">
-        <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-        </button>
+const SearchBar = ({ onSubmit }) => {
+    const [query, setQuery] = useState('');
 
-        <input
-            className="SearchForm-input"
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-        />
-    </form>
-</header>
+    const handleChange = e => {
+        setQuery(e.currentTarget.value);
+    };
 
-export default Searchbar;
+    const handleSubmit = e => {
+        e.preventDefault();
+        onSubmit(query);
+        setQuery('');
+        e.currentTarget.reset();
+    };
+
+    return (
+        <header className="Searchbar">
+            <form className="SearchForm" onSubmit={handleSubmit}>
+                <button type="submit" className="SearchForm-button">
+                    <span className="SearchForm-button-label">Search</span>
+                </button>
+
+                <input
+                    className="SearchForm-input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                    onChange={handleChange}
+                />
+            </form>
+        </header>
+    );
+};
+
+SearchBar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
