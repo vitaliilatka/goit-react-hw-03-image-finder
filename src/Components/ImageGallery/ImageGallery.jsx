bef1c8df5c784999cc7a73bff846e2a8d06c2a71
vetallatka <latka.vitalik@yandex.ru>
2021-04-11 07:32:14 +0200

    added imageButton

--- src/Components/ImageGallery/ImageGallery.jsx
+++ src/Components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import ImageGalleryItem from '../ImageGalleryItem';
 import PropTypes from 'prop-types';
 
-<ul className="ImageGallery">
-    <!-- Набор <li> с изображениями -->
-</ul>
+const ImageGallery = ({ images, onClick }) => {
+    return (
+        <ul className="ImageGallery">
+            {images.map(({ id, webformatURL, largeImageURL }) => (
+                <ImageGalleryItem
+                    onClick={onClick}
+                    key={id}
+                    webformatURL={webformatURL}
+                    largeImageURL={largeImageURL}
+                />
+            ))}
+        </ul>
+    );
+};
 
+ImageGallery.propTypes = {
+    images: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+        }),
+    ).isRequired,
+};
 
 export default ImageGallery;
